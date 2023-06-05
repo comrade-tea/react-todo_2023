51d6fa4f419d75ce18572595c2d9d18ff5e5856b
comrade-tea_github <vasil@dotorg.ru>
2023-06-05 23:46:49 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,9 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/react-todo_2023/",
+    base: "./react-todo_2023/",
     // base: process.NODE_ENV === "production" ? "/vite-ts/" : "/",
-    // publicDir
-    // : "public",
+    // publicDir: "public",
     plugins: [react()],
     resolve: {
 
